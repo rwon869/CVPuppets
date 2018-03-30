68a1fc0ecd1c28abfcb230a88a9ec66f82e3f254
Noah D'Souza <noah.dsouza@students.olin.edu>
2018-03-30 14:26:06 -0400

    Started FirstDraft

---
+++ First_Draft/firstDraft.py
@@ -0,0 +1,20 @@
+import OpenCV
+import KinectInterfacer
+
+class Kinect():
+
+    def __init__():
+        #put necessary variables here
+
+    def run_cam():
+        #run the Kinect's IR camera
+
+    def receive_data():
+        #save the data temporarily for tracking
+        #will likely call run_cam()
+
+    def tracking():
+        #call receive_data()
+        #read the data and interperet it 
+
+class
